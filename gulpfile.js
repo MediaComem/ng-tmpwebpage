38cd858740da8c5bc2ac30f4548e19fc8dbd269a
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-08-08 14:50:13 +0200

    Browsersync setup with html and sass file changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,18 @@ var browserSync = require('browser-sync').create();
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('serve', function() {
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
+  gulp.watch("./sass/**/*.scss", ['sass'])
+  gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+gulp.task('default', ['serve']);
