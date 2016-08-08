928af18c744dbb1e78c15fee3a69412a0cc50581
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-08-08 14:04:54 +0200

    Browser sync basic usage

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
 
 gulp.task('sass:watch', function () {
